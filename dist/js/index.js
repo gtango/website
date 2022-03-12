5aad1bdd02ae8b13e2a0010781d3dcf63dc9fccf
Gavril Tango <gavriltango@gavrils-mbp.lan>
2022-03-12 12:22:08 -0500

    Adding toggle close on button

--- dist/js/index.js
+++ dist/js/index.js
@@ -1,5 +1,14 @@
 const navToggle = document.querySelector('.nav--toggle');
+const navLink = document.querySelectorAll('.nav__item');
 
 navToggle.addEventListener('click', () => {
     document.body.classList.toggle('nav-open');
 });
+
+console.log(navLink);
+
+navLink.forEach(item => {
+    item.addEventListener('click', () => {
+        document.body.classList.remove('nav-open'); 
+    })
+})
