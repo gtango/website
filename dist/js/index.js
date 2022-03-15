2d7d7d28b8e31eb9122daf6e3472d2ff8ff52188
Gavril Tango <gavriltango@gavrils-mbp.lan>
2022-03-14 20:40:07 -0400

    Minor changes to asthetic

--- dist/js/index.js
+++ dist/js/index.js
@@ -1,14 +1,26 @@
+// Navigation js
 const navToggle = document.querySelector('.nav--toggle');
+const navMove = document.querySelector('.nav--movable');
 const navLink = document.querySelectorAll('.nav__item');
+const home = document.getElementById('home');
+const homePos = home.getBoundingClientRect();
 
 navToggle.addEventListener('click', () => {
     document.body.classList.toggle('nav-open');
 });
 
-console.log(navLink);
-
 navLink.forEach(item => {
     item.addEventListener('click', () => {
         document.body.classList.remove('nav-open'); 
     })
+})
+
+window.addEventListener("scroll", () =>{
+    if(window.scrollY > homePos.height){
+        navMove.style.opacity = '1';
+        navMove.style.visibility = 'visible';
+    } else {
+        navMove.style.opacity = '0';
+        navMove.style.visibility = 'hidden';
+    }
 })
